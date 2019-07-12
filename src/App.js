97baf15830e5e8f0af7704d70d356b884b883b07
Philipp <github@phibography.de>
2019-07-12 20:50:25 +0200

    range input with timestamps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const datesArray = []
+  const minDate = Date.parse('2019-07-12 00:00')
+  const maxDate = Date.now()
+  let date = minDate
+  while (date < maxDate) {
+    datesArray.push(date)
+    date = date + 1000 * 60 * 15
+  }
+
+  const [timestamp, setTimestamp] = React.useState(minDate)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { _formatDate(timestamp) }
+      <input type="range"
+        min={datesArray[0]}
+        max={datesArray[datesArray.length - 1]}
+        step={1000 * 60 * 15}
+        onInput={event => { setTimestamp(Number(event.currentTarget.value)) }}
+        style={{
+          width: '100%'
+        }}
+      />
     </div>
   );
+}
+
+const _formatDate = (timestamp) => {
+  const d = new Date(timestamp)
+  return d.toLocaleString()
 }
 
 export default App;
